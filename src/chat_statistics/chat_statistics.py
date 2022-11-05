67e8223d1de1432153ab9d0fe49d8677fe372036
iamem1r <amiryamini95@gmail.com>
2022-11-05 21:36:50 +0330

    add logger.info

--- src/chat_statistics/chat_statistics.py
+++ src/chat_statistics/chat_statistics.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 from typing import Union
+from loguru import logger
 
 import arabic_reshaper
 from bidi.algorithm import get_display
@@ -10,18 +11,22 @@ from wordcloud import WordCloud
 
 
 class ChatStatistics:
+    """Generates chat statistics
+    """
     def __init__(self, file_path: Union[str, Path]) -> None:
         """read a text file(.json or .txt)
 
         :param file_path: path to the file
         :type file_path: Union[str, Path]
         """
+        logger.info("Reading chat data from: %s" % file_path)
         with open(file_path) as f:
             self.data = json.load(f)
 
         self.normalizer = Normalizer()
 
         # load stop words
+        logger.info("load stop words from: %s" % file_path)
         stopwords = open(DATA_DIR / 'stopwords.txt').readlines()
         stopwords = list(map(str.strip, stopwords))
         self.stopwords = list(map(self.normalizer.normalize, stopwords))
@@ -36,6 +41,7 @@ class ChatStatistics:
         :return: save .png to output_file_path
         :rtype: .png
         """
+        logger.info("Loading text content...")
         text_str = ''
         text_list = ''
         message = self.data['messages']
@@ -61,18 +67,22 @@ class ChatStatistics:
         text_content = text_str + ' ' + text_list
 
         #reshape, and normalize text content
+        logger.info("Reshape and normalizing text content")
         text = self.normalizer.normalize(text_content)
         text = arabic_reshaper.reshape(text[:500000])
         text = get_display(text)
 
         # generate wordcloud
+        logger.info("Generating wordcloud...")
         wordcloud = WordCloud(
         font_path=str(DATA_DIR / 'Mitra_Bold.ttf'),
-        width=800,
-        height=400,
+        width=1800,
+        height=1200,
         background_color='white'
         ).generate(text)
 
         # save to output_file_path
+        logger.info("Saving wordcloud to: %s" % output_file_path)
         wordcloud.to_file(str(Path(output_file_path) / 'wordcloud.png'))
+        logger.info("Done.")
 
--- src/main.py
+++ src/main.py
@@ -6,4 +6,3 @@ from src.chat_statistics.chat_statistics import ChatStatistics
 if __name__ == '__main__':
     chat_data = ChatStatistics(file_path=DATA_DIR / 'CS-Stack.json')
     chat_data.generate_word_cloud(output_file_path=DATA_DIR)
-    print("Done!")
