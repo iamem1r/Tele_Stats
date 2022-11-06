8d750de33302c569448d2de8b691572adb4e721e
iamem1r <amiryamini95@gmail.com>
2022-11-06 15:45:49 +0330

    bug fixed

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from src.data import DATA_DIR
 from src.chat_statistics.chat_statistics import ChatStatistics
 
 
-
 if __name__ == '__main__':
     chat_data = ChatStatistics(file_path=DATA_DIR / 'CS-Stack.json')
     chat_data.generate_word_cloud(output_file_path=DATA_DIR)
