4123d8830087ccf92b5954c857172599f368c1a7
iamem1r <amiryamini95@gmail.com>
2022-11-06 16:55:09 +0330

    bug fixed

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-from src.data import DATA_DIR
 from src.chat_statistics.chat_statistics import ChatStatistics
-
+from src.data import DATA_DIR
 
 if __name__ == '__main__':
     chat_data = ChatStatistics(file_path=DATA_DIR / 'CS-Stack.json')
